4ae3fc70901a33ed8f80b62299811818f0ede632
Cywka333 <j.cywinski95@gmail.com>
2022-10-08 17:31:42 +0200

    First commit

---
+++ src/pl/devsmentoring/Main.java
@@ -0,0 +1,20 @@
+package pl.devsmentoring;
+
+public class Main {
+    public static void main(String[] args) {
+       // String companyName = "Devs-Mentoring", brandName = "Devs-Hunting";
+        // System.out.println(companyName.compareTo(brandName));
+
+        //int value = 99;
+        //String isEven = value % 2 == 0 ? "parzysta" : "nieparzysta";
+        //System.out.println(isEven);
+
+        char letter_k = 'k', letter_j = 'j';
+        if(letter_k > letter_j){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO!");
+        }
+    }
+}
